c00e53a063a1fbae330e794fd20c237792281fe9
Ibtid <ibtid011@example.com>
2023-08-25 21:57:16 +0600

    List Messages done

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import BloodRequestPage from './pages/BloodRequestPage';
+import DoctorsPage from './pages/DoctorsPage';
+import HomePage from './pages/HomePage';
+import ListMessages from './pages/ListMessages';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <AccountPage /> */}
-      {/* <AccountInfoPage /> */}
-      {/* <HomePage /> */}
-      {/* <HospitalDetailPage /> */}
-      {/* <DiagnosticCenterDetailPage /> */}
-      {/* <CrowdFundingApplicationPage /> */}
-      <BloodRequestPage />
+      <ListMessages />
     </View>
   );
 }
