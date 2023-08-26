c4f7a80f906411e35980db6299a49ffef60a2ad0
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-08-26 19:27:53 +0600

    dental centers page done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import NumberVerificationPage from "./pages/NumberVerificationPage";
+import DentalCentersPage from "./pages/DentalCentersPage";
 
 export default function App() {
   return (
@@ -18,7 +18,8 @@ export default function App() {
       {/* <BloodBanksPage /> */}
       {/* <ContactRequestPage /> */}
       {/* <FundSomeonePage /> */}
-      <NumberVerificationPage />
+      {/* <NumberVerificationPage /> */}
+      <DentalCentersPage />
     </View>
   );
 }
